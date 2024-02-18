82e80985d16821effa29a5ff2ad5a651753390ee
Reneechang17 <136089488+Reneechang17@users.noreply.github.com>
2024-02-17 20:01:03 -0500

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -26,11 +26,24 @@ export const updateUser = async (req, res, next) => {
           avatar: req.body.avatar,
         },
       },
-      { new: true })
+      { new: true }
+    );
 
     const { password, ...rest } = updatedUser._doc;
 
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
