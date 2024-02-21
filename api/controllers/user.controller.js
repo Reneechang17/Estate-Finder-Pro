1ec6d63cd4c8c8151f8a3ce0e124f7d5db0339af
Reneechang17 <136089488+Reneechang17@users.noreply.github.com>
2024-02-21 09:34:55 -0500

    add contact landlord functionality to the listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,7 +3,6 @@ import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import Listing from "../models/listing.model.js";
 
-
 export const test = (req, res) => {
   res.json({
     message: "API route is working!",
@@ -61,5 +60,16 @@ export const getUserListings = async (req, res, next) => {
     }
   } else {
     return next(errorHandler(401, "You can only view your own listing!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return next(errorHandler(404, "User not found!"));
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
